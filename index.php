9fb56ceb61048ffe7769e2a959e5c94a848cad24
Caua Galvao <notgamer30900@gmail.com>
2024-05-23 17:56:48 -0300

    Oficina GitHub

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boas-vindas no teste da oficina Github</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 100px;
+        }
+
+        h1 {
+            color: #333;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Bem-vindo ao meu site!</h1>
+    <?php
+
+    echo "<p>Hoje é " . date("d/m/Y") . "</p>";
+    ?>
+</body>
+
+</html>
